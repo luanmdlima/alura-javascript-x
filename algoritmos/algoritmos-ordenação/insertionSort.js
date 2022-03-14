e11719b54c4dc3d4f964ef34c6cc7250cc0dd825
Luan M. D. Lima <luanmdlima.pro@gmail.com>
2022-03-14 12:07:44 -0300

    Projeto inicial; Algoritmos com JavaScriot II: aprofundando em algoritmos de ordenacao e busca

--- algoritmos/algoritmos-ordenação/insertionSort.js
+++ algoritmos/algoritmos-ordenação/insertionSort.js
@@ -1,12 +1,12 @@
-const livros = require('./listaLivros');
-const troca = require('./troca');
+const livros = require("./listaLivros");
+const troca = require("./troca");
 
 function insertionSort(lista) {
   for (let atual = 0; atual < lista.length; atual++) {
     let analise = atual;
     while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
       troca(lista, analise);
-      analise--
+      analise--;
     }
   }
   console.log(lista);
--- algoritmos/algoritmos-ordenação/menorValor.js
+++ algoritmos/algoritmos-ordenação/menorValor.js
@@ -1,11 +1,11 @@
-const livros = require('./listaLivros');
+const livros = require("./listaLivros");
 
 function menorValor(arrProdutos, posicaoInicial) {
   let maisBarato = posicaoInicial;
-  
+
   for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
     if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
-      maisBarato = atual
+      maisBarato = atual;
     }
   }
   return maisBarato;
